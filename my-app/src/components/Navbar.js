3f20a79e13d7f1b37d7a44eec6f5eb2c51fd6238
shreayasinghkunwar <shreaya@fusemachines.com>
2022-12-02 13:55:11 +0545

    nav

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -31,7 +31,7 @@ const Navbar = () => {
         <header>
             <nav class="navbar navbar-expand-lg ">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="#"><img class="logo" src={logo} /></a>
+                    <Link class="navbar-brand" href="#"><img class="logo" src={logo} /></Link>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
